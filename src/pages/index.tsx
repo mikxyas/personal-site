26f7e97f75877c3fb91a973ad2004ccf1a214fdf
Mikiyas <mymikiaa11@gmail.com>
2023-10-07 10:12:29 +0300

    removed quotes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     <main
       className={`flex min-h-screen flex-col items-center justify-center  p-24 ${inter.className}`}
     >
-      "Man is something that shall be overcome"
+      Man is something that shall be overcome
     </main>
   )
 }
